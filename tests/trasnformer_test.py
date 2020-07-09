e0e89f7b526882a08d227c7067f24b0feff54225
rapidlizard <kiko.cbg@gmail.com>
2020-07-09 17:08:40 +0200

    refactor transformer tests

--- tests/trasnformer_test.py
+++ tests/trasnformer_test.py
@@ -30,31 +30,8 @@ def test_user_transformer_turns_json_dict_into_user_obj():
     user = User_transformer().transform(data)
 
     assert type(user) == User
-
-def test_user_transformer_turns_json_dict_into_user_atributes():
-    data = {
-        "steamid": "76561198066000502",
-        "communityvisibilitystate":3,
-        "profilestate":1,
-        "personaname":"Lixard",
-        "commentpermission":1,
-        "profileurl":"https://steamcommunity.com/profiles/76561198066000502/",
-        "avatar":"https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/4a/4ad45031967e52ce05f28c7f5591227e66715c5d.jpg",
-        "avatarmedium":"https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/4a/4ad45031967e52ce05f28c7f5591227e66715c5d_medium.jpg",
-        "avatarfull":"https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/4a/4ad45031967e52ce05f28c7f5591227e66715c5d_full.jpg",
-        "avatarhash":"4ad45031967e52ce05f28c7f5591227e66715c5d",
-        "lastlogoff":1594245076,
-        "personastate":0,
-        "primaryclanid":"103582791463737200",
-        "timecreated":1340730740,
-        "personastateflags":4,
-        "loccountrycode":"PT",
-        "locstatecode":"14"
-    }
-
-    user = User_transformer().transform(data)
-
     assert user.name == 'Lixard'
     assert user.img == 'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/4a/4ad45031967e52ce05f28c7f5591227e66715c5d_full.jpg'
     assert user.url == 'https://steamcommunity.com/profiles/76561198066000502/'
     assert user.created == '1340730740'
+
