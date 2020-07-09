3e68633b659c67fc937bd8845b7c499a6cb23f57
rapidlizard <kiko.cbg@gmail.com>
2020-07-09 14:05:42 +0200

    remove data wrapping in steam client, add more tests for steam client

--- models/api_client.py
+++ models/api_client.py
@@ -4,4 +4,7 @@ class Steam():
     def get_user():
         url = "http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=2FA14ED02A1D7CCC0E4FCA80AE6AE194&steamids=76561198066000502"
         response = requests.get(url)
-        return response.json()
+
+        data = response.json()
+
+        return data['response']['players'][0]
--- tests/api_client_test.py
+++ tests/api_client_test.py
@@ -5,6 +5,39 @@ sys.path.append(parentdir)
 
 from models.api_client import Steam
 
-def test_steam_returns_user_data():
+def test_steam_client_returns_json():
     data = Steam.get_user()
+    
     assert type(data) == dict
+
+def test_steam_client_returns_user_data_without_wrapping():
+    expected = {
+        "steamid":"76561198066000502",
+        "communityvisibilitystate":3,
+        "profilestate":1,
+        "personaname":"Lixard",
+        "commentpermission":1,
+        "profileurl":"https://steamcommunity.com/profiles/76561198066000502/",
+        "avatar":"https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/4a/4ad45031967e52ce05f28c7f5591227e66715c5d.jpg",
+        "avatarmedium":"https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/4a/4ad45031967e52ce05f28c7f5591227e66715c5d_medium.jpg",
+        "avatarfull":"https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/4a/4ad45031967e52ce05f28c7f5591227e66715c5d_full.jpg",
+        "avatarhash":"4ad45031967e52ce05f28c7f5591227e66715c5d",
+        "lastlogoff":1594245076,
+        "personastate":0,
+        "primaryclanid":"103582791463737200",
+        "timecreated":1340730740,
+        "personastateflags":4,
+        "loccountrycode":"PT",
+        "locstatecode":"14"
+    }
+
+    data = Steam.get_user()
+
+    assert data == expected
+
+def test_steam_client_returns_correct_user():
+    expected = '76561198066000502'
+
+    data = Steam.get_user()
+
+    assert data['steamid'] == expected
