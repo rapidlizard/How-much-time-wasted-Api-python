0af66a51c78bceceb6021c971006ef561b8b0387
rapidlizard <kiko.cbg@gmail.com>
2020-07-09 15:08:43 +0200

    refactor /howmuchtimehaveiwasted route handler

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 from flask import Flask, jsonify
 from models.user import User
+from models.api_client import Steam
+from models.transformers import User_transformer
 
 app = Flask(__name__)
 
-@app.route('/howmuchtimehaveiwasted')
-def get_user():
-    new_user = User('lizard', 'img', 'url', 'now')
-    
-    return jsonify(new_user.to_json())
+@app.route('/howmuchtimehaveiwasted/<steamid>')
+def get_user(steamid):
+    data = Steam.get_user(steamid)
+
+    user = User_transformer().transform(data)
+
+    return jsonify(user.to_json())
 
 if __name__ == '__main__':
     app.run()
--- tests/app_test.py
+++ tests/app_test.py
@@ -10,13 +10,19 @@ import pytest
 def client():
     return app.test_client()
 
-def test_api_returns_user_json(client):
-    response = client.get('/howmuchtimehaveiwasted')
+@pytest.fixture
+def steamid():
+    return "76561198066000502"
+
+def test_api_returns_user_json(client, steamid):
+    response = client.get('/howmuchtimehaveiwasted/' + steamid)
+
+    assert response.status_code == 200
     assert response.get_json() == {
-        'name': 'lizard',
-        'img': 'img',
-        'url': 'url',
-        'created': 'now'
+        'name': 'Lixard',
+        'img': 'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/4a/4ad45031967e52ce05f28c7f5591227e66715c5d_full.jpg',
+        'url': 'https://steamcommunity.com/profiles/76561198066000502/',
+        'created': '1340730740'
     }
 
 
