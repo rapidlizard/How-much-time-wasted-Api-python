e0e52738e96acef3a74f073c57cd5951abe5fdbf
rapidlizard <kiko.cbg@gmail.com>
2020-07-09 14:55:45 +0200

    user transformer now creates user obj from data

--- models/transformers.py
+++ models/transformers.py
@@ -3,4 +3,10 @@ from models.user import User
 class User_transformer():
     
     def transform(self, data):
-        return User('jeff', 'img', 'url', 'now')
+        user = User(
+            name= data['personaname'],
+            img= data['avatarfull'],
+            url= data['profileurl'],
+            created= str(data['timecreated'])
+        )
+        return user 
--- tests/trasnformer_test.py
+++ tests/trasnformer_test.py
@@ -8,10 +8,23 @@ from models.user import User
 
 def test_user_transformer_turns_json_dict_into_user_obj():
     data = {
-        'name': 'jeff',
-        'img': 'img',
-        'url': 'url',
-        'created': 'now'
+        "steamid": "76561198066000502",
+        "communityvisibilitystate":3,
+        "profilestate":1,
+        "personaname":"Lixard",
+        "commentpermission":1,
+        "profileurl":"https://steamcommunity.com/profiles/76561198066000502/",
+        "avatar":"https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/4a/4ad45031967e52ce05f28c7f5591227e66715c5d.jpg",
+        "avatarmedium":"https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/4a/4ad45031967e52ce05f28c7f5591227e66715c5d_medium.jpg",
+        "avatarfull":"https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/4a/4ad45031967e52ce05f28c7f5591227e66715c5d_full.jpg",
+        "avatarhash":"4ad45031967e52ce05f28c7f5591227e66715c5d",
+        "lastlogoff":1594245076,
+        "personastate":0,
+        "primaryclanid":"103582791463737200",
+        "timecreated":1340730740,
+        "personastateflags":4,
+        "loccountrycode":"PT",
+        "locstatecode":"14"
     }
 
     user = User_transformer().transform(data)
@@ -20,15 +33,28 @@ def test_user_transformer_turns_json_dict_into_user_obj():
 
 def test_user_transformer_turns_json_dict_into_user_atributes():
     data = {
-        'name': 'jeff',
-        'img': 'img',
-        'url': 'url',
-        'created': 'now'
+        "steamid": "76561198066000502",
+        "communityvisibilitystate":3,
+        "profilestate":1,
+        "personaname":"Lixard",
+        "commentpermission":1,
+        "profileurl":"https://steamcommunity.com/profiles/76561198066000502/",
+        "avatar":"https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/4a/4ad45031967e52ce05f28c7f5591227e66715c5d.jpg",
+        "avatarmedium":"https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/4a/4ad45031967e52ce05f28c7f5591227e66715c5d_medium.jpg",
+        "avatarfull":"https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/4a/4ad45031967e52ce05f28c7f5591227e66715c5d_full.jpg",
+        "avatarhash":"4ad45031967e52ce05f28c7f5591227e66715c5d",
+        "lastlogoff":1594245076,
+        "personastate":0,
+        "primaryclanid":"103582791463737200",
+        "timecreated":1340730740,
+        "personastateflags":4,
+        "loccountrycode":"PT",
+        "locstatecode":"14"
     }
 
     user = User_transformer().transform(data)
 
-    assert user.name == 'jeff'
-    assert user.img == 'img'
-    assert user.url == 'url'
-    assert user.created == 'now'
+    assert user.name == 'Lixard'
+    assert user.img == 'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/4a/4ad45031967e52ce05f28c7f5591227e66715c5d_full.jpg'
+    assert user.url == 'https://steamcommunity.com/profiles/76561198066000502/'
+    assert user.created == '1340730740'
