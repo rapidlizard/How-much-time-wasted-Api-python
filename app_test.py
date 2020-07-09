15275a57100c0949c8535861a7d6ab4b071e9173
rapidlizard <kiko.cbg@gmail.com>
2020-07-09 10:34:19 +0200

    rename routes_test to app_test, add model User

--- app_test.py
+++ app_test.py
@@ -1,4 +1,5 @@
 from app import app
+from models.user import User
 import pytest
 
 @pytest.fixture
@@ -9,3 +10,6 @@ def test_api_returns_steam_id(client):
     response = client.get('/howmuchtimehaveiwasted/42069')
     assert response.data == b'Your steam id is:42069'
 
+def test_user_has_atribute_steam_id():
+    user = User('123')
+    assert user.steam_id == '123'
---
+++ models/user.py
@@ -0,0 +1,4 @@
+class User():
+    
+    def __init__(self, steam_id):
+        self.steam_id = steam_id
