937157b45019d9c9c4dc96577c65898e060475ee
rapidlizard <kiko.cbg@gmail.com>
2020-07-14 15:48:08 +0200

    add steam api client test for csgo gun stats

--- tests/api_client_test.py
+++ tests/api_client_test.py
@@ -121,3 +121,41 @@ def test_steam_client_returns_csgo_stats_data_with_correct_attributes(steamid):
     assert 'total_shots_fired' in data
     assert 'total_shots_hit' in data
     assert 'total_deaths' in data
+
+
+def test_steam_returns_csgo_stats_with_correct_gun_stats(steamid):
+    data = Steam().get_user_csgo_stats(steamid)
+
+    assert "total_kills_hegrenade" in data
+    assert "total_kills_glock" in data
+    assert "total_kills_deagle" in data
+    assert "total_kills_elite" in data
+    assert "total_kills_fiveseven" in data
+    assert "total_kills_xm1014" in data
+    assert "total_kills_mac10" in data
+    assert "total_kills_ump45" in data
+    assert "total_kills_p90" in data
+    assert "total_kills_awp" in data
+    assert "total_kills_ak47" in data
+    assert "total_kills_aug" in data
+    assert "total_kills_famas" in data
+    assert "total_kills_g3sg1" in data
+    assert "total_kills_m249" in data
+    assert "total_kills_hkp2000" in data
+    assert "total_kills_p250" in data
+    assert "total_kills_sg556" in data
+    assert "total_kills_scar20" in data
+    assert "total_kills_ssg08" in data
+    assert "total_kills_mp7" in data
+    assert "total_kills_mp9" in data
+    assert "total_kills_nova" in data
+    assert "total_kills_negev" in data
+    assert "total_kills_sawedoff" in data
+    assert "total_kills_bizon" in data
+    assert "total_kills_tec9" in data
+    assert "total_kills_mag7" in data
+    assert "total_kills_m4a1" in data
+    assert "total_kills_galilar" in data
+    assert "total_kills_molotov" in data
+    assert "total_kills_decoy" in data
+    assert "total_kills_taser" in data
